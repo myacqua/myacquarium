dd57d3fb173231f4c5c87740389aec1d064dc8db
Michele Criscuolo <crisc.mic@gmail.com>
2020-11-07 17:07:09 +0100

    create tue entry per l'array pesci per test di ricerca

--- src/app/services/appstate.service.ts
+++ src/app/services/appstate.service.ts
@@ -8,5 +8,34 @@ export class AppStateService {
   public acquarioCorrente: any = {};
   public state: string = "";
 
-  constructor() { }
+  //  oggetti acquario
+  public pesci: any[] = [];
+  public piante: any[] = [];
+  public accessori: any[] = [];
+
+  constructor() { 
+
+    this.pesci = [
+      {
+        nome: "Carassio dorato o pesce rosso",
+        dominio: "Eukaryota",
+        regno: "Animalia",
+        phylum: "Chordata",
+        classe: "Actinopterygii",
+        ordine: "Perciformes",
+        famiglia: "Cyprinidae",
+        genere: "Carassius"
+      },
+      {
+        nome: "Scalare",
+        dominio: "Eukaryota",
+        regno: "Animalia",
+        phylum: "chordata",
+        classe: "Actinopterygii",
+        ordine: "Perciformes",
+        famiglia: "Ciclidae",
+        genere: "Pterophyllum"
+      }
+    ];
+  }
 }
