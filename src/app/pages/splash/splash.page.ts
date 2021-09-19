13011c47512f242678325bc7ac9cfe42bf0b92c7
Michele Criscuolo <crisc.mic@gmail.com>
2021-09-19 19:56:17 +0200

    aggiornamento ionic e fix splash

--- src/app/pages/splash/splash.page.ts
+++ src/app/pages/splash/splash.page.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
-
+import { VascheService } from '../../_services/vasche.service';
 @Component({
   selector: 'app-splash',
   templateUrl: './splash.page.html',
@@ -8,12 +8,22 @@ import { Router } from '@angular/router';
 })
 export class SplashPage implements OnInit {
 
-  constructor(private router:Router) { }
+  array_vasche: any = [];
+
+  constructor(private router:Router, private vascheService: VascheService) { }
 
   ngOnInit() {
     this.router.navigate(['/dashboard'], { replaceUrl: true });
   }
 
   ionViewWillEnter() {
+  }
+
+  vascaOnclick(vasca: any) {
+    
+  }
+
+  getVascheService() {
+    return this.vascheService;
   }
 }
