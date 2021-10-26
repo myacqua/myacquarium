4905f975875a177b89dd6c91f99483070a554434
Michele Criscuolo <crisc.mic@gmail.com>
2021-10-26 22:30:35 +0200

    aggiunta la ricerca della pianta e dei pesci man mano che si scrive

--- src/app/components/ricerca-pesce/ricerca-pesce.component.ts
+++ src/app/components/ricerca-pesce/ricerca-pesce.component.ts
@@ -1,8 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AppStateService } from 'src/app/_services/appstate.service';
 import { PesciService } from 'src/app/_services/pesci.service';
 import { AlertService } from 'src/app/_services/alert.service';
+import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ricerca-pesce',
@@ -10,6 +12,8 @@ import { AlertService } from 'src/app/_services/alert.service';
   styleUrls: ['./ricerca-pesce.component.scss'],
 })
 export class RicercaPesceComponent implements OnInit {
+
+  @ViewChild('searchInput', { read: ElementRef }) searchInput: ElementRef;
 
   array_pesci: any = [];
 
@@ -25,17 +29,33 @@ export class RicercaPesceComponent implements OnInit {
 
   ngOnInit() { 
 
+    //  TODO_MICHELE: attenzione a controllare quando diventa disponibile la search box
+    setTimeout(() => {
+      
+      //  mi registro all'evento di keyup della barra di ricerca
+      fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
+        map((i: any) => i.currentTarget.value),
+        debounceTime(800)
+      ).subscribe(() => this.onClickSearch())
+    }, 1000);
+
   }
+
+
 
   /**
    * Fa una ricerca per un pesce in base ai parametri di ricerca
    */
-  processForm(form: NgForm) {
+  processForm() {
 
     this.formProcessed = true;
+    
+    //  TODO_MICHELE: aggiungere la cancellazione dei filtri
+    if (this.modelSearch.nomePesce.length < 3)
+      return;
 
     this.pesciService.ricercaPesce(this.modelSearch, this.appstate.currentVasca, (response) => {
-      if(response.length>0){
+      if(response.length > 0){
         this.array_pesci = response;
       } else {
         this.notify.showNotification("Nessun pesce trovato", 'danger');
@@ -43,6 +63,12 @@ export class RicercaPesceComponent implements OnInit {
     });
     
   }
+
+
+  onClickSearch() {
+    this.processForm();
+  }
+
 
   /**
    * Va nel dettaglio del pesce selezionato
--- src/app/components/ricerca-piante/ricerca-piante.component.ts
+++ src/app/components/ricerca-piante/ricerca-piante.component.ts
@@ -1,9 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 import { AppStateService } from 'src/app/_services/appstate.service';
 import { PianteService } from 'src/app/_services/piante.service';
 import { NgForm } from '@angular/forms';
 import { AlertService } from 'src/app/_services/alert.service';
-
+import { fromEvent } from 'rxjs';
+import { debounceTime, map } from 'rxjs/operators';
 
 @Component({
   selector: 'app-ricerca-piante',
@@ -12,11 +13,11 @@ import { AlertService } from 'src/app/_services/alert.service';
 })
 export class RicercaPianteComponent implements OnInit {
 
+  @ViewChild('searchInput', { read: ElementRef }) searchInput: ElementRef;
+
   array_piante: any = [];
 
   formProcessed: boolean = false;
-
-
   
     //  Oggetto per la ricerca di una pianta
   public modelSearch = {
@@ -25,17 +26,31 @@ export class RicercaPianteComponent implements OnInit {
   }
 
   constructor(private notify: AlertService, private appstate:AppStateService, private pianteService: PianteService) { }
-   
+
   ngOnInit() {
 
+    //  TODO_MICHELE: attenzione a controllare quando diventa disponibile la search box
+    setTimeout(() => {
+      
+      //  mi registro all'evento di keyup della barra di ricerca
+      fromEvent(this.searchInput.nativeElement, 'keyup').pipe(
+        map((i: any) => i.currentTarget.value),
+        debounceTime(800)
+      ).subscribe(() => this.onClickSearch())
+    }, 1000);
   }
+
 
   /**
    * Fa una ricerca per una pianta in base ai parametri di ricerca
    */
-  processForm(form: NgForm) {
+  processForm() {
 
     this.formProcessed = true;
+
+    //  TODO_MICHELE: aggiungere la cancellazione dei filtri
+    if (this.modelSearch.nomePiante.length < 3)
+      return;
 
     this.pianteService.ricercaPiante(this.modelSearch, this.appstate.currentVasca, (response) => {
       if(response.length>0){
@@ -44,6 +59,10 @@ export class RicercaPianteComponent implements OnInit {
         this.notify.showNotification("Nessuna pianta trovata", 'danger');
       }
     });
+  }
+
+  onClickSearch() {
+    this.processForm();
   }
 
   /**
