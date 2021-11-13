e25e1bb0437dc8677acb61c5ea23434b050a8d5c
alessandro.paolillo <alessandro.paolillo@gmail.com>
2021-11-13 19:07:49 +0100

    Fix grafiche

--- src/app/pages/selezione-oggetto/selezione-oggetto.page.ts
+++ src/app/pages/selezione-oggetto/selezione-oggetto.page.ts
@@ -4,6 +4,7 @@ import { AlertService } from 'src/app/_services/alert.service';
 import { AppStateService } from 'src/app/_services/appstate.service';
 import { VascheService } from 'src/app/_services/vasche.service';
 import { LoadingService } from 'src/app/_services/loading.service';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-selezione-oggetto',
@@ -15,7 +16,7 @@ export class SelezioneOggettoPage implements OnInit {
   private loadingInProgress: boolean = false;
 
   constructor(private notify: AlertService, private appState:AppStateService,private vascheService:VascheService, private router: Router,
-                private loading: LoadingService, private alert: AlertService) { }
+                private loading: LoadingService, private alert: AlertService, private alertController: AlertController) { }
 
   ngOnInit() {
   
@@ -36,35 +37,79 @@ export class SelezioneOggettoPage implements OnInit {
     }
   }
 
+  showConfirm(messageAlert) {
+    this.alertController.create({
+      header: 'Conferma Inserimento',
+      subHeader: messageAlert,
+      message: 'Sei sicuro di voler aggiungere un Vivo?',
+      buttons: [
+        {
+          text: 'No',
+          handler: () => {
+            console.log('Let me think');
+          }
+        },
+        {
+          text: 'SI',
+          handler: () => {
+            if (this.appState.currentPesce != null) {
+              /**INSERISCO PESCE**/
+              this.vascheService.aggiungiPesce(this.appState.currentVasca, this.appState.currentPesce, (success) => {
+                this.notify.showNotification("Pesce aggiunto alla vasca","success");
+                this.router.navigate(['gestisci-acquario']);
+              }, (error) => {
+                //  TODO aggiungere il messaggio di errore che arriva dal backend
+                console.log(error);
+                this.setLoading(false);
+                this.alert.showError("Impossibile aggiungere il pesce", "");
+              });
+            } else if (this.appState.currentPiante != null) {
+              /**INSERISCO PIANTA**/
+              this.vascheService.aggiungiPianta(this.appState.currentVasca, this.appState.currentPiante, (success) => {
+                this.notify.showNotification("Pianta aggiunta alla vasca", "success");
+                this.router.navigate(['gestisci-acquario']);
+              }, 
+              (error) => {
+                console.log(error.message);
+                this.setLoading(false);
+                this.alert.showError("Impossibile aggiungere la pianta", error.message);
+              });
+            }
+          }
+        }
+      ]
+    }).then(res => {
+      res.present();
+    });
+  }
+
   addVivo (){
     
     if ( this.appState.canAdd ) {
       /**INSERISCO**/
-      if (this.appState.currentPesce != null) {
-        /**INSERISCO PESCE**/
-        this.vascheService.aggiungiPesce(this.appState.currentVasca, this.appState.currentPesce, (success) => {
-          this.notify.showNotification("Pesce aggiunto alla vasca","success");
-          this.router.navigate(['gestisci-acquario']);
-        }, (error) => {
-          //  TODO aggiungere il messaggio di errore che arriva dal backend
-          console.log(error);
-          this.setLoading(false);
-          this.alert.showError("Impossibile aggiungere il pesce", "");
-        });
-      } else if (this.appState.currentPiante != null) {
-        /**INSERISCO PIANTA**/
-        this.vascheService.aggiungiPianta(this.appState.currentVasca, this.appState.currentPiante, (success) => {
-          this.notify.showNotification("Pianta aggiunta alla vasca", "success");
-          this.router.navigate(['gestisci-acquario']);
-        }, 
-        (error) => {
-          console.log(error.message);
-          this.setLoading(false);
-          this.alert.showError("Impossibile aggiungere la pianta", error.message);
-        });
-      }
+      var message = "";
+      if (this.appState.currentPesce!=null) {
+        if (this.appState.currentPesce.volumeAlert || this.appState.currentPesce.temperatureAlert || 
+              this.appState.currentPesce.phAlert || this.appState.currentPesce.ghAlert) {
+          if (this.appState.currentPesce.volumeAlert) message = "Il volume della vasca non consente di inserire il pesce";
+          else if (this.appState.currentPesce.phAlert) message = "Il PH della vasca non consente di inserire il pesce";
+          else if (this.appState.currentPesce.ghAlert) message = "Il GH della vasca non consente di inserire il pesce";
+          else if (this.appState.currentPesce.temperatureAlert) message = "La temperatura della vasca non consente di inserire il pesce";
+        } 
+      } else if (this.appState.currentPiante!=null) {
+        if (this.appState.currentPiante.khAlert || this.appState.currentPiante.temperatureAlert || 
+            this.appState.currentPiante.phAlert || this.appState.currentPiante.ghAlert) {
+          if (this.appState.currentPiante.khAlert) message = "Il KH della vasca non consente di inserire la pianta";
+          else if (this.appState.currentPiante.phAlert) message = "Il PH della vasca non consente di inserire la pianta";
+          else if (this.appState.currentPiante.ghAlert) message = "Il GH della vasca non consente di inserire la pianta";
+          else if (this.appState.currentPiante.temperatureAlert) message = "La temperatura della vasca non consente di inserire la pianta";
+        }
+      } 
+      this.showConfirm(message);
     }
   }
+
+  
 
   loadData(event) {
 
